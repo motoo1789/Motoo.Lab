1d3bec1155fc416dd6fb19118c929edd23523b91
motoo1789 <53427863+motoo1789@users.noreply.github.com>
2019-12-13 10:10:11 +0900

    Add files via upload

---
+++ js/タブ.js
@@ -0,0 +1,11 @@
+$(function() {
+    $('.tab_btn').on('click', function(){
+        
+        $('.tab_item').removeClass('is-active-item');
+        $($(this).attr("href")).addClass('is-active-item');
+
+        $('.tab_btn').removeClass('is-active-btn');
+        $(this).addClass('is-active-btn');
+    });
+});
+
